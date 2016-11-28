254422fb11d3be99474cc27acf75a6699eca0ed6
J Ridgeway <sw333zy@gmail.com>
2016-11-28 13:01:43 -0500

    first commit

---
+++ js/storeData.module.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+angular.module('store')
+  .controller('storeDataController', storeDataController);
+
+function storeDataController() {
+  console.log('Im making a store data controller');
+
+}
+
+
+
+
+
+}());
