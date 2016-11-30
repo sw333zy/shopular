58a37160e7f8fa2169e48f5eef0ed2fd2c3f383e
J Ridgeway <sw333zy@gmail.com>
2016-11-30 15:53:16 -0500

    added local storage w/ page refresh

--- js/product.service.js
+++ js/product.service.js
@@ -1,10 +1,134 @@
 (function() {
-  'use strict';
-  angular.module('store')
-    .factory('ProductService', ProductService)
+    'use strict';
+    angular.module('store')
+        .factory('ProductService', ProductService)
     console.log('making a ProductService');
 
-    function ProductService() {
+    var storedItems = JSON.parse(localStorage.getItem('inventory'));
+    console.log(storedItems);
 
+    function ProductService() {
+        var inventory = storedItems || [{
+            "id": 2957,
+            "name": "widget",
+            "price": 32,
+            "quantity": 203,
+            "color": "red",
+            "discount": 31
+        }, {
+            "id": 89274,
+            "name": "golf club",
+            "price": 98,
+            "quantity": 10,
+            "color": "black",
+            "discount": 0
+        }, {
+            "id": 64,
+            "name": "iPhone",
+            "price": 499,
+            "quantity": 2,
+            "color": "white",
+            "discount": 0
+        }, {
+            "id": 87363,
+            "name": "bonzai tree",
+            "price": 76,
+            "quantity": 2,
+            "color": "green",
+            "discount": 0
+        }, {
+            "id": 1736,
+            "name": "towel",
+            "price": 55,
+            "quantity": 30,
+            "color": "brown",
+            "discount": 10
+        }, {
+            "id": 4836,
+            "name": "dog bed",
+            "price": 99,
+            "quantity": 10,
+            "color": "beige",
+            "discount": 50
+        }, {
+            "id": 829,
+            "name": "waste basket",
+            "price": 15,
+            "quantity": 40,
+            "color": "silver",
+            "discount": 0
+        }, {
+            "id": 46,
+            "name": "guitar",
+            "price": 899,
+            "quantity": 0,
+            "color": "blue",
+            "discount": 150
+        }, {
+            "id": 17456,
+            "name": "matcha tea",
+            "price": 42,
+            "quantity": 4,
+            "color": "green",
+            "discount": 11
+        }, {
+            "id": 3292,
+            "name": "enlightenment",
+            "price": 99999,
+            "quantity": 1,
+            "color": "red",
+            "discount": 0
+        }, {
+            "id": 533,
+            "name": "eggs",
+            "price": 5,
+            "quantity": 12,
+            "color": "brown",
+            "discount": 1
+        }, {
+            "id": 683,
+            "name": "pillow",
+            "price": 27,
+            "quantity": 10,
+            "color": "black",
+            "discount": 12
+        }];
+
+
+
+    return {
+        getAll: getAll,
+        addNew: addNew,
+        update: update
+
+    };
+
+
+
+
+    // this object below IS the service
+
+    function getAll() {
+        return inventory;
     }
+
+    function addNew(product) {
+        inventory.push({
+            id: product.id,
+            name: product.name,
+            price: Number(product.price),
+            quantity: Number(product.quantity),
+            color: product.color,
+            discount: Number(product.discount)
+        });
+        update();
+    }
+
+    function update(){
+      localStorage.setItem('inventory', angular.toJson(inventory));
+    }
+
+  };
+
+
 }());
